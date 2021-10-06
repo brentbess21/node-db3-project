5ac524e8d133c079cec5a00ae8941d74f2b11aef
brentbess21 <brentbess21@gmail.com>
2021-10-06 14:06:54 -0600

    initial commit. Passing tests 1-4

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const Scheme = require('./scheme-model');
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,17 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const realScheme = await Scheme.findById(req.params.id)
+    if(!realScheme) {
+      next({status: 404, message: `scheme with scheme_id ${req.params.id} not found`})
+    } else {
+      console.log("Happy Path for checkScheme ID")
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
