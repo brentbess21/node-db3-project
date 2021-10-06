a3c3b8b9275c7367072af951335a1d5001ccd6ba
brentbess21 <brentbess21@gmail.com>
2021-10-06 14:29:26 -0600

    Wrote validateScheme middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -11,7 +11,10 @@ const checkSchemeId = async (req, res, next) => {
   try {
     const realScheme = await Scheme.findById(req.params.id)
     if(!realScheme) {
-      next({status: 404, message: `scheme with scheme_id ${req.params.id} not found`})
+      next({
+        status: 404, 
+        message: `scheme with scheme_id ${req.params.id} not found`
+      })
     } else {
       console.log("Happy Path for checkScheme ID")
     }
@@ -29,7 +32,15 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if( scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()) {
+    next({ 
+      status: 400,
+      message: "invalid scheme_name"
+    })
+  } else {
+    next()
+  }
 }
 
 /*
